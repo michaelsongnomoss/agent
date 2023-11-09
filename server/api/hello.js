45cc92e162fa310ed7c71a4099accc8efbf3027d
anduril_nomoss <michaelsong@nomoss.co>
2023-11-10 00:12:36 +1100

    delete unused comments

--- server/api/hello.js
+++ server/api/hello.js
@@ -15,7 +15,6 @@ import {
   });
 export default defineEventHandler(async (event) => {
 
-    // const query = "when was the paper written?"
     const body = await readBody(event)
     const query = body.prompt
     console.log("---------query")
@@ -25,18 +24,13 @@ export default defineEventHandler(async (event) => {
     const model = new OpenAI();
 
       const template = `
-      The following is a friendly conversation between a human and an AI. 
-      The AI is talkative and provides lots of specific details from its context. 
       If the AI does not know the answer to a question, it truthfully says it does not know.
-      Use the "history" to understand what we've already talked about in the conversation.
   
       Use the CONTEXT below to answer the QUESTION asked by the user.
       `
-        // const humanTemplate = "{text}";
 
         const chatPrompt = ChatPromptTemplate.fromMessages([
         ["system", template]
-        // ["human", humanTemplate],
         ]);
 
       const llmChain = new ConversationChain({
@@ -59,27 +53,19 @@ export default defineEventHandler(async (event) => {
       const pineconeResponse = await vectorChain.call({ query });
 
       try {
-        // Create a prompt to inform the LLM how to respond
-        // Insert the database results and question to guide the answer
         const prompt = `
               CONTEXT: ${JSON.stringify(pineconeResponse)}
               
               QUESTION: ${query}
               `;
   
-        // Await the LLM response
+
         const response = await llmChain.call({ input: prompt });
-        // console.log(response)
-        // return response;
-        // return response
-        // Return the result to requester
-        // response.status(200).json({ response });
+
       } catch (err) {
         console.log(err)
-        // response.status(500).json({ error: 'Failed to load data' });
       }
 
-    //   console.log(response);
       return { pineconeResponse };
 
 });
